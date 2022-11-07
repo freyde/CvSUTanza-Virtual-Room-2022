71d7baf5204ced80ef60a3bf2dba425842c881ee
freyde <34171677+freyde@users.noreply.github.com>
2022-11-07 12:02:29 +0800

    Cleanup: BSBM Area VII Parameter A

---
+++ flipbook/bsba/area8/paramB/s-2/mobile/javascript/search_config.js
@@ -0,0 +1,1 @@
+﻿var textForPages = ["S.2.            The buildings are constructed                           according to their respective uses.","The Buildings are constructed according to their respective uses."];
