d24e431ef3431cc093a08484e44fb606de3ef384
lalabsko17 <61335778+lalabsko17@users.noreply.github.com>
2022-11-09 08:43:30 +0800

    BSE AREA 2 PARAM A flip error fixed

--- flipbook/bse/area2/paramA/s-1-4-1/mobile/javascript/text_position[16].js
+++ flipbook/bse/area2/paramA/s-1-4-1/mobile/javascript/text_position[16].js
@@ -1,1 +1,1 @@
-﻿positionForPages[16]={page:16,positions:[{w:"",p:[0.117688,0.107513,0.121750,0.107513,0.121750,0.093624,0.117688,0.093624,0.004062]}]};
+﻿positionForPages[16]={page:16,positions:[]};
