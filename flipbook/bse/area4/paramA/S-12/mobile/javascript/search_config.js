42decbdf84b9f12f176753226313a594e990e626
freyde <34171677+freyde@users.noreply.github.com>
2022-11-14 14:45:16 +0800

    update

--- flipbook/bse/area4/paramA/S-12/mobile/javascript/search_config.js
+++ flipbook/bse/area4/paramA/S-12/mobile/javascript/search_config.js
@@ -1,1 +1,1 @@
-﻿var textForPages = ["S.12.           There is sufficient budget allocation for                           the Student Affairs and Service of the                           institution.","","","","","","","","","","","","","","","","","","","",""];
+﻿var textForPages = ["S.12.           There is sufficient budget allocation for                           the Student Affairs and Service of the                           institution.","","OSAS Financial Plan","","","","","","","","","","","","","","","","","","","",""];
