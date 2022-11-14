e4734eac4a23fd5851f6ea2b194112a23b6a5b59
freyde <34171677+freyde@users.noreply.github.com>
2022-11-14 18:09:50 +0800

    update

--- flipbook/bse/area5/paramC/i-1/mobile/javascript/text_position[8].js
+++ flipbook/bse/area5/paramC/i-1/mobile/javascript/text_position[8].js
@@ -1,1 +1,1 @@
-﻿positionForPages[8]={page:8,positions:[]};
+﻿positionForPages[8]={page:8,positions:[{w:"",p:[0.918005,0.891360,0.922180,0.891360,0.922180,0.878296,0.918005,0.878296,0.004175]}]};
--- flipbook/bse/area5/paramC/i-1/mobile/javascript/text_position[9].js
+++ flipbook/bse/area5/paramC/i-1/mobile/javascript/text_position[9].js
@@ -1,1 +1,1 @@
-﻿positionForPages[9]={page:9,positions:[]};
+﻿positionForPages[9]={page:9,positions:[{w:"",p:[0.885069,0.858551,0.889244,0.858551,0.889244,0.845487,0.885069,0.845487,0.004175]}]};
