1a317177f7e646727279aba0400393e0a7e33f57
freyde <34171677+freyde@users.noreply.github.com>
2022-11-14 14:22:11 +0800

    update

--- flipbook/bsba/area4/paramB/s-3-2/mobile/javascript/search_config.js
+++
@@ -1,1 +0,0 @@
-﻿var textForPages = ["S.3.            The information materials on                           educational, career, personal and                           social concerns are readily available,                          such as:                           S.3.2.  Anti-Bullying Act;","","Manual of Operations","","","Student Handbook","","","",""];
