f47326ae83c9542594a0e11e9151d8a8033277f3
jhnalvin <116610481+justine1030@users.noreply.github.com>
2022-11-17 00:04:50 +0800

    Week 4 upload

--- flipbook/bse/area2/paramH/i-1-5/mobile/javascript/text_position[93].js
+++ flipbook/bse/area2/paramH/i-1-5/mobile/javascript/text_position[93].js
@@ -1,1 +1,1 @@
-﻿positionForPages[93]={page:93,positions:[{w:"",p:[0.000119,0.987962,0.007093,0.987962,0.007093,0.968184,0.000119,0.968184,0.006974]}]};
+﻿positionForPages[93]={page:93,positions:[]};
